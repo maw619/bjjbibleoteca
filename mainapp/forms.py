e0ea40b571375656c21f525e8c2e6bc72f9b0512
maw619 <marcowolff619@gmail.com>
2026-04-19 17:15:51 -0400

    updated email

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 ALLOWED_EMAILS = [
     "friend1@gmail.com",
-    "friend2@gmail.com",
+    "chanelmizani@gmail.com",
     "marcowolff619@gmail.com",
 ]
 
@@ -15,7 +15,7 @@ class RegisterForm(UserCreationForm):
         email = self.cleaned_data.get("email")
 
         if email not in ALLOWED_EMAILS:
-            raise forms.ValidationError("ask Chanel for info")
+            raise forms.ValidationError("ask Chanel for info/password")
 
         return email
 
