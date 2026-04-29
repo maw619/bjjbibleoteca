8f2c3214adfec7fac8c367d3e9844880f0a164aa
Marco Wolff <marcowolff619@gmail.com>
2026-04-29 17:26:58 -0400

    Add merge migrations for mainapp 0005 conflict

---
+++ mainapp/migrations/0006_merge_0005_alter_note_options_0005_notecomment.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mainapp', '0005_alter_note_options'),
+        ('mainapp', '0005_notecomment'),
+    ]
+
+    operations = []
